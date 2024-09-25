d79235f244980cee39f65d59eb1e0b5da8542adb
aditya-dev07 <singhaditya9946@gmail.com>
2024-09-25 11:27:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Quiz from "./Quiz-Components/Quiz";
+
+const App = () => {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+};
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    question: "Which company developed React.JS?",
+    option1: "Apple",
+    option2: "Facebook",
+    option3: "Google",
+    option4: "Twitter",
+    ans: 2,
+  },
+  {
+    question:
+      "Which of the following terms commonly described react applications?",
+    option1: "Imperative",
+    option2: "Integrated",
+    option3: "Declarative",
+    option4: "closed",
+    ans: 3,
+  },
+  {
+    question: "In which language  is React.JS written?",
+    option1: "Python",
+    option2: "JavaScript",
+    option3: "Java",
+    option4: "PHP",
+    ans: 2,
+  },
+  {
+    question: "Which of the following is not a method of redux store?",
+    option1: "getState()",
+    option2: "subscribe()",
+    option3: "dispatch()",
+    option4: "render()",
+    ans: 4,
+  },
+  {
+    question:
+      "Which of the following is used to pass data to a component from outside in React?",
+    option1: "props",
+    option2: "render with arguments",
+    option3: "setState",
+    option4: "PropTypes",
+    ans: 1,
+  },
+];
